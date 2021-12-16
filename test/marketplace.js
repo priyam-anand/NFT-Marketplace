386ebbcc34c6006097c1bc9e698edbaae3f8afbb
Priyam Anand <priyam27.anand@gmail.com>
2021-12-16 11:07:18 +0530

    tests complete

--- test/marketplace.js
+++ test/marketplace.js
@@ -41,7 +41,7 @@ contract("Marketplace", function (accounts) {
     const tokenId = (token.logs[2].args[0]).toNumber();
 
     expectRevert(
-      marketplace.createItem(item.address,tokenId,{value:250000000000000,from:accounts[1]}),
+      marketplace.createItem(item.address, tokenId, { value: 250000000000000, from: accounts[1] }),
       "Can only add your own token"
     )
   })
@@ -49,20 +49,20 @@ contract("Marketplace", function (accounts) {
   it("should create Item", async () => {
     const token = await item.createToken("https://sampleLink1.com");
     const tokenId = (token.logs[2].args[0]).toNumber();
-    await marketplace.createItem(item.address,tokenId,{value:250000000000000,from:accounts[0]});
-    
+    await marketplace.createItem(item.address, tokenId, { value: 250000000000000, from: accounts[0] });
+
     const itemId = await marketplace.itemID();
     assert(itemId.toNumber() === 2);
   })
 
-  it("should not list Item if not owner",async()=>{
+  it("should not list Item if not owner", async () => {
     const token = await item.createToken("https://sampleLink1.com");
     const tokenId = (token.logs[2].args[0]).toNumber();
-    await marketplace.createItem(item.address,tokenId,{value:250000000000000,from:accounts[0]});
-    
+    await marketplace.createItem(item.address, tokenId, { value: 250000000000000, from: accounts[0] });
+
     const itemId = await marketplace.itemID();
     expectRevert(
-      marketplace.listItem(itemId.toNumber()-1,500,{from:accounts[1]}),
+      marketplace.listItem(itemId.toNumber() - 1, 500, { from: accounts[1] }),
       "Only item owner can list"
     )
   });
@@ -70,11 +70,39 @@ contract("Marketplace", function (accounts) {
   it("should list Item", async () => {
     const token = await item.createToken("https://sampleLink1.com");
     const tokenId = (token.logs[2].args[0]).toNumber();
-    await marketplace.createItem(item.address,tokenId,{value:250000000000000,from:accounts[0]});
-    
-    const itemId = await marketplace.itemID();
-    await marketplace.listItem(itemId.toNumber()-1,500,{from:accounts[0]}),
-    assert(true);
-  })
+    await marketplace.createItem(item.address, tokenId, { value: 250000000000000, from: accounts[0] });
 
+    const itemId = await marketplace.itemID();
+    await marketplace.listItem(itemId.toNumber() - 1, 500, { from: accounts[0] });
+    assert(true);
+  });
+
+  it("should not sell if not listed", async () => {
+    const token = await item.createToken("https://sampleLink1.com");
+    const tokenId = (token.logs[2].args[0]).toNumber();
+    await marketplace.createItem(item.address, tokenId, { value: 250000000000000, from: accounts[0] });
+
+    const itemId = (await marketplace.itemID()).toNumber() - 1;
+
+    expectRevert(
+      marketplace.sellItem(item.address, itemId),
+      "Item is not listed on the market"
+    )
+  });
+
+  it("should sell item", async () => {
+    const token = await item.createToken("https://sampleLink1.com");
+    const tokenId = (token.logs[2].args[0]).toNumber();
+    await marketplace.createItem(item.address, tokenId, { value: 250000000000000, from: accounts[0] });
+
+    const itemId = (await marketplace.itemID()).toNumber() - 1;
+    await marketplace.listItem(itemId, 500, { from: accounts[0] });
+
+    const iniBal = web3.utils.toBN(await web3.eth.getBalance(accounts[0]));
+    await marketplace.sellItem(item.address, itemId, { from: accounts[1], value: 500 });
+    const finalBal = web3.utils.toBN(await web3.eth.getBalance(accounts[0]));
+    const newOwner = await item.ownerOf(tokenId);
+    const dif = (finalBal.sub(iniBal));
+    assert(dif.toNumber() === 250000000000499 && newOwner.toLowerCase() === accounts[1].toLowerCase())
+  });
 });
